09066b6537752339c833d33e8b53d8cf48a790cc
Helgesen Håvard <helgesenhb@gmail.com>
2019-03-06 10:45:00 +0100

    fix

---
+++ js/nim.js
@@ -0,0 +1,28 @@
+function Nim(player1, player2, victory, total maxGrab) {
+
+player1 = {name: "name", human: True},
+player2 = {name: "name", human: True}.
+victory: function() {
+    if (nim == finish) {
+        return finish + vinner;
+    }
+}.
+total: 10; (heltall) // Antall felles klinkekuler i starten. 
+maxGrab: 1; (heltall) if not an argument, maxGrab == 3.
+
+
+
+
+
+
+
+
+
+this.player1 = player1;
+this.player2 = player2;
+this.victory = victory;
+this.total = total;
+this.maxGrab = maxGrab;
+
+
+}
