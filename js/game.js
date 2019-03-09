e9db2ab5d21ca368abbfb67ff26d99d5c9cf528e
henrikbonsaksen <henrik_bon@hotmail.com>
2019-03-09 12:17:33 +0100

    color_adjustments

--- js/game.js
+++ js/game.js
@@ -1,37 +1,34 @@
-// Skal definere logikk som oppretter et "Nim Objekt".
+// // Skal definere logikk som oppretter et "Nim Objekt".
+//
+//
+// // Skal returnere verdier:
+// var protoSpiller = {
+//   get gamer() {
+//     return this.name + "is a: " + " " this.human + "player " + "and has: " + this.total + "left";
+//   }
+// };
+//
+// // Oppskrift på å lage spillere:
+// function createPlayer(name, human, total) {
+//   var spillerObjekt = Object.create(opprettSpillere);
+//   spillerObjekt.name = name;
+//   spillerObjekt.human = human;
+//   spillerObjekt.total = total;
+//   return spillerObjekt;
+// }
+//
+// // SKal definere et nytt spill:
+// var spill = {
+//   game: createPlayer("Geir", True, 14)
+// };
+//
+// // Loop for å teste stuff... yeeeaaahhhh.. ghaaaddd..
+// for (var nøkkel in spill) {
+//   var verdi = spill[nøkkel];
+//   console.log(verdi.gamer);
+// }
+//
 
 
-// Skal returnere verdier:
-var protoSpiller = {
-  get gamer() {
-    return this.name + "is a: " + " " this.human + "player " + "and has: " + this.total + "left";
-  }
-};
-
-// Oppskrift på å lage spillere:
-function createPlayer(name, human, total) {
-  var spillerObjekt = Object.create(opprettSpillere);
-  spillerObjekt.name = name;
-  spillerObjekt.human = human;
-  spillerObjekt.total = total;
-  return spillerObjekt;
-}
-
-// SKal definere et nytt spill:
-var spill = {
-  game: createPlayer("Geir", True, 14)
-};
-
-// Loop for å teste stuff... yeeeaaahhhh.. ghaaaddd..
-for (var nøkkel in spill) {
-  var verdi = spill[nøkkel];
-  console.log(verdi.gamer);
-}
-
-
-function changeState() {
-   var buttons = document.getElementById(".button1");
-   buttons.innerHTML = '<img src="../img/buttn.jpg" />';
-}
 
 // var x = document.getElementById("myImg").alt;
--- js/nim.js
+++ js/nim.js
@@ -1,28 +1,28 @@
-// Konstruktør som oppretter et objekt som representerer et Nim spill:
-function Nim(player1, player2, victory, total, maxGrab) {
-  this.player1 = player1;
-  this.player2 = player2;
-  this.victory = victory;
-  this.total = total;
-  this.maxGrab = maxGrab;
-}
-
-
-// Kladd under:
-
-function Nim(player1, player2, victory, total, maxGrab) {
-  player1 = {name: "Geir", human: True},
-  player2 = {name: "Svein", human: True},
-  victory: function() {
-    return this.player + "is the: " + " " + this.result;
-  }
-
-  total: 14;
-
-
-}
-this.player1 = player1;
-this.player2 = player2;
-this.victory = victory;
-this.total = total;
-this.maxGrab = maxGrab;
+// // Konstruktør som oppretter et objekt som representerer et Nim spill:
+// function Nim(player1, player2, victory, total, maxGrab) {
+//   this.player1 = player1;
+//   this.player2 = player2;
+//   this.victory = victory;
+//   this.total = total;
+//   this.maxGrab = maxGrab;
+// }
+//
+//
+// // Kladd under:
+//
+// function Nim(player1, player2, victory, total, maxGrab) {
+//   player1 = {name: "Geir", human: True},
+//   player2 = {name: "Svein", human: True},
+//   victory: function() {
+//     return this.player + "is the: " + " " + this.result;
+//   }
+//
+//   total: 14;
+//
+//
+// }
+// this.player1 = player1;
+// this.player2 = player2;
+// this.victory = victory;
+// this.total = total;
+// this.maxGrab = maxGrab;
