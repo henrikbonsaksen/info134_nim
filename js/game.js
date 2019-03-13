83ad4cc95c25eac6a07bd058170fea4aba1d7704
Helgesen Håvard <helgesenhb@gmail.com>
2019-03-13 10:39:47 +0100

    update_js

--- js/game.js
+++ js/game.js
@@ -1,34 +1,5 @@
-// // Skal definere logikk som oppretter et "Nim Objekt".
-//
-//
-// // Skal returnere verdier:
-// var protoSpiller = {
-//   get gamer() {
-//     return this.name + "is a: " + " " this.human + "player " + "and has: " + this.total + "left";
-//   }
-// };
-//
-// // Oppskrift på å lage spillere:
-// function createPlayer(name, human, total) {
-//   var spillerObjekt = Object.create(opprettSpillere);
-//   spillerObjekt.name = name;
-//   spillerObjekt.human = human;
-//   spillerObjekt.total = total;
-//   return spillerObjekt;
-// }
-//
-// // SKal definere et nytt spill:
-// var spill = {
-//   game: createPlayer("Geir", True, 14)
-// };
-//
-// // Loop for å teste stuff... yeeeaaahhhh.. ghaaaddd..
-// for (var nøkkel in spill) {
-//   var verdi = spill[nøkkel];
-//   console.log(verdi.gamer);
-// }
-//
+// Denne filen skal bruke Nim objektet for å
+// lage nye instanser av konstruktøren something.
 
-
-
-// var x = document.getElementById("myImg").alt;
+// Lager et nytt Nim objekt.
+var n = new Nim("Geir", "Svein", victory, 14, 2);
--- js/nim.js
+++ js/nim.js
@@ -1,28 +1,26 @@
-// // Konstruktør som oppretter et objekt som representerer et Nim spill:
-// function Nim(player1, player2, victory, total, maxGrab) {
-//   this.player1 = player1;
-//   this.player2 = player2;
-//   this.victory = victory;
-//   this.total = total;
-//   this.maxGrab = maxGrab;
-// }
-//
-//
-// // Kladd under:
-//
-// function Nim(player1, player2, victory, total, maxGrab) {
-//   player1 = {name: "Geir", human: True},
-//   player2 = {name: "Svein", human: True},
-//   victory: function() {
-//     return this.player + "is the: " + " " + this.result;
-//   }
-//
-//   total: 14;
-//
-//
-// }
-// this.player1 = player1;
-// this.player2 = player2;
-// this.victory = victory;
-// this.total = total;
-// this.maxGrab = maxGrab;
+
+// This is a constructor function that initializes new Nim objects.
+// It does not create or return them, just initializes them.
+function Nim(player1, player2, victory, total, /* optional */ maxGrab) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.victory = victory;
+    this.total = total;
+    this.maxGrab = maxGrab;
+}
+
+// All Nim objects inherit from this object.
+Nim.prototype = {
+    player1 = {name: "", human: True}; // objekt med navn og om spilleren er menneske.
+    player2 = {name: "", human: True}. // objekt med navn og om spilleren er menneske.
+    victory: function() { // Når spillet er avsluttet, skal spillobjektet kalle denne.
+        if (Nim == finish)
+        return finish + vinner;
+    }.
+
+    total: 10; (heltall) // total skal være et heltall.
+
+    // Hvis maxGrab ikke blir gitt som argument, skal den returnere 3.
+    if (maxGrab == undefined) maxGrab = 3;
+
+};
