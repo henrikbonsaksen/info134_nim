1f63ec7179606ad438a6dcda42e124c85c1d8267
henrikbonsaksen <henrik_bon@hotmail.com>
2019-03-14 15:35:22 +0100

    pusherman

--- js/nim.js
+++ js/nim.js
@@ -9,19 +9,60 @@ function Nim(player1, player2, victory, total, maxGrab) {
     this.maxGrab = maxGrab;
 }
 
+var game = new Nim("p1", "p2", 1, 5, 3)
 
 // All Nim objects inherit from this object.
 
-var player1 = new Nim (name: "", human: True); // objekt med navn og om spilleren er menneske.
-var player2 = new Nim (name: "", human: True); // objekt med navn og om spilleren er menneske.
+// var player1 = new Nim (name: "", human: True); // objekt med navn og om spilleren er menneske.
+// var player2 = new Nim (name: "", human: True); // objekt med navn og om spilleren er menneske.
 
-function victory() { // Når spillet er avsluttet, skal spillobjektet kalle denne.
-        if (Nim == finish)
-        return finish + "vinner";
+// Når spillet er avsluttet, skal spillobjektet kalle denne.
+
+
+var total = game.total;
+var maxgrab = game.maxGrab;
+var player1 = game.player1;
+var player2 = game.player2;
+var victory = game.victory;
+
+function Victory(player) {
+  console.log(player + "vinner");
+};
+
+
+function remove1() {
+  total-=1;
+  maxgrab-=1;
+  console.log("fra", game.total,"til ",total)
+  console.log(maxgrab)
+  if (total === victory) {
+    Victory("player1 ");
+  }
+};
+
+function remove2() {
+  total-=2;
+  maxgrab-=1;
+  console.log("fra", game.total,"til ",total)
+  console.log(maxgrab)
+  if (total === victory) {
+    Victory("player1 ");
+  }
+};
+
+function remove3() {
+  total-=3;
+  maxgrab-=1;
+  console.log("fra", game.total,"til ",total)
+  console.log(maxgrab)
+  if (total === victory) {
+    Victory("player1 ");
+  }
 };
 
 
 
+// document.getElementById("score").innerHTML = total;
 
 
 //     total: 10;  // total skal være et heltall.//
